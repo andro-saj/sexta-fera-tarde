cd6eb8ac885d0aa46f18cab3e8b445648bb7cd51
andro-saj <71135059+andro-saj@users.noreply.github.com>
2022-09-30 10:50:12 -0300

    kjkds
    sd

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-$(function(){
+$(function() {
 
-    $("navbarToggle").blur(function (event) {
+    $("#navbarToggle").blur(function (event) {
        
        var screenWidth = window.innerWidth;
-       if(screenWidth<768){
+       if( screenWidth < 768){
            $("#collapsable-nav").collapse('hide');
        }
 
