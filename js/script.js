791f2f22a4a1aeefa070b8be0ec004aa393ad460
Disk Vendas SAJ <71135059+andro-saj@users.noreply.github.com>
2025-12-24 19:58:25 -0300

    jjjj

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-$(function () { // Same as document.addEventListener("DOMContentLoaded"...
-
-  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+$(function () { 
+  // 1. Fecha o menu ao perder o foco (Blur)
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
     if (screenWidth < 768) {
@@ -8,12 +7,17 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     }
   });
 
-  // In Firefox and Safari, the click event doesn't retain the focus
-  // on the clicked button. Therefore, the blur event will not fire on
-  // user clicking somewhere else in the page and the blur event handler
-  // which is set up above will not be called.
-  // Refer to issue #28 in the repo.
-  // Solution: force focus on the element that the click event fired on
+  // 2. Solução para fechar ao clicar em qualquer lugar da página (Mobile)
+  $(document).click(function (event) {
+    var clickTarget = $(event.target);
+    var _opened = $("#collapsable-nav").hasClass("in"); // No BS3, 'in' indica que está aberto
+    
+    if (_opened === true && !clickTarget.hasClass("navbar-toggle")) {
+      $("#navbarToggle").click();
+    }
+  });
+  
+  // 3. Garante o foco para o blur funcionar no Safari/Firefox
   $("#navbarToggle").click(function (event) {
     $(event.target).focus();
   });
